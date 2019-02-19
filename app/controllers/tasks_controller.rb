47a492818ef371802869e93e6888453c14752eaf
Alexander Seyss <a@seyss.at>
2019-02-19 17:10:44 +0000

    create, edit set up

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,38 @@
 class TasksController < ApplicationController
-
+  before_action :set_task, only: [:show, :edit, :update]
   def index
     @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task = @task.update(task_params)
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  patch 'tasks/:id', to: 'tasks#update'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
 end
